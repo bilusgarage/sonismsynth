7ea6226bfc7e14d71d9dc9b465d80724fbe425f7
bbilu56 <161428500+bilusgarage@users.noreply.github.com>
2026-05-01 20:51:10 +0200

    fix: update plugin name

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -15,10 +15,9 @@ TEST_CASE ("Plugin instance", "[instance]")
     SECTION ("name")
     {
         CHECK_THAT (testPlugin.getName().toStdString(),
-            Catch::Matchers::Equals ("Pamplejuce Demo"));
+            Catch::Matchers::Equals ("sonism"));
     }
 }
-
 
 #ifdef PAMPLEJUCE_IPP
     #include <ipp.h>
@@ -26,10 +25,10 @@ TEST_CASE ("Plugin instance", "[instance]")
 TEST_CASE ("IPP version", "[ipp]")
 {
     #if defined(__APPLE__)
-        // macOS uses 2021.9.1 from pip wheel (only x86_64 version available)
-        CHECK_THAT (ippsGetLibVersion()->Version, Catch::Matchers::Equals ("2021.9.1 (r0x7e208212)"));
+    // macOS uses 2021.9.1 from pip wheel (only x86_64 version available)
+    CHECK_THAT (ippsGetLibVersion()->Version, Catch::Matchers::Equals ("2021.9.1 (r0x7e208212)"));
     #else
-        CHECK_THAT (ippsGetLibVersion()->Version, Catch::Matchers::Equals ("2022.3.0 (r0x0fc08bb1)"));
+    CHECK_THAT (ippsGetLibVersion()->Version, Catch::Matchers::Equals ("2022.3.0 (r0x0fc08bb1)"));
     #endif
 }
 #endif
